3e59df1da60a0e9ca076420dd11b64dc7c5acd13
Ashish <53848069+ash-ys@users.noreply.github.com>
2022-01-04 12:10:28 +0545

    p

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React from 'react'
 import CardList from "./components/card-list.component";
 import Search from "./components/search.component";
 import Title from "./components/title.component";
